0cb4c1964037e0e3d53cd37b44cf43be7ef9cdef
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-05-15 17:40:26 +0900

    00_03

--- WinApp.h
+++ WinApp.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <Windows.h>
+#include <cstdint>
+
 class WinApp
 {
-};
+public:
+	//ウィンドウサイズ
+	static const int32_t kClientWidth = 1280;//横幅
+	static const int32_t kClientHeight = 720;//縦幅
 
+	//ウィンドウプロシージャ
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+	static void CreateWindowView(const wchar_t* title = L"CG2_WinMain");
+
+private:
+	// ウィンドウクラス
+	static inline WNDCLASS wc{};
+
+	//ウィンドウオブジェクト
+	static HWND hwnd;
+};
