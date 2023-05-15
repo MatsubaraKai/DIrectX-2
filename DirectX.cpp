affdcade82cb2b3d8bbc0dfabecffdce5529c2c2
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-05-15 14:35:32 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectX.cpp
@@ -0,0 +1,15 @@
+#include "DirectX.h"
+#include <Windows.h>
+#include <cstdint>
+#include <string>
+#include <format>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <dxgidebug.h>
+#include <dxcapi.h>
+#include "Vector4.h"
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib,"dxguid.lib")
+#pragma comment(lib,"dxcompiler.lib")
